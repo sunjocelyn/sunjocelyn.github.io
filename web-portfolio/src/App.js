7d2721cc3da361eadbeab401d0461495db10c53b
sunjocelyn <jwsun@uchicago.edu>
2025-09-13 01:37:02 -0400

    committed in the wrong directory, adding files that were missed

--- web-portfolio/src/App.js
+++ web-portfolio/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import NavPanel from "./components/header";
+import HighlightedWorks from "./components/highlightedworks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavPanel />
+      <hr style={{width: "100%", height: "0.24rem", backgroundColor:"#FFD478", margin: "0", border: "none"}}></hr>
+      <hr style={{width: "100%", height: "0.09rem", backgroundColor:"#000000", margin: "0", border: "none"}}></hr>
+      <body>
+        <HighlightedWorks />
+      </body>
     </div>
   );
 }
